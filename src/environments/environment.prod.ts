f470de306c20e5d0bb899fcdc4995c27a47e5358
Juan David Martinez <juandavidmartinez576@gmail.com>
2024-10-03 17:02:14 -0500

    Firebae_config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyDyQc9PwOpW3gO4q7bjtck-bsHnhIWGREE",
+    authDomain: "agendatareadb.firebaseapp.com",
+    projectId: "agendatareadb",
+    storageBucket: "agendatareadb.appspot.com",
+    messagingSenderId: "326230871216",
+    appId: "1:326230871216:web:3bcc82859b37ce254b7f75",
+    measurementId: "G-D1QP108CWP"
+  }
 };
